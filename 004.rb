a05c5379d5ffa8adc5fb7b8cac37c31443bea2d1
shadai <shadai@movomx.com>
2024-06-27 15:17:28 -0700

    004 fixed

--- 004.rb
+++ 004.rb
@@ -1,32 +1,20 @@
 # Escriba un algoritmo que tome el siguiente texto y devuelva el número de
 # caracteres y el número de palabras en dicho texto.
 
-input = 'El sentido de la birria'
+koan = File.open('data/koan.txt','r').read
 
-def to_characters_array(text)
-  text_array = text.downcase.split('')
-  text_array.delete(' ')
-  text_array
+def count_characters(text)
+  text.size
 end
 
-def to_words_array(text)
-  words_array = text.downcase.split(' ')
-end
-
-def character_counter(word)
-  characters_array = to_characters_array(word)
-  characters_array.count
-end
-
-def words_counter(word)
-  words_array = to_words_array(word)
-  words_array.count
+def count_words(text)
+  text.split(' ').size
 end
 
 def count_characters_words(word)
-  total_characters = character_counter(word)
-  total_words = words_counter(word)
+  total_characters = count_characters(word)
+  total_words = count_words(word)
   puts "la palabra contiene #{total_words} palabras y #{total_characters} caracteres"
 end
 
-count_characters_words(input)
+count_characters_words(koan)
--- 005.rb
+++ 005.rb
@@ -3,14 +3,26 @@
 # Ninguna palabra debe quedar truncada.
 
 def read_from_user
-  input = 'Esto es un texto'
+  input = 'Escriba un algoritmo que tome un texto del usuario y devuelva un array de strings, tal que ninguno de sus elementos pase de los 140 caracteres. Ninguna palabra debe quedar truncada, carateres de mas'
   #input = gets.chomp
+  check_array(input)
 end
 
-def convert_to_array(word)
-  text = word.split(/\A.{0,9}[a-z]\b/mi)
+def check_array(text)
+  text_array = text.split
+  character_counter = 0
+  text = ''
+  text_array.each do |word|
+    if character_counter < 140
+      character_counter = text.insert( -1, " #{word}").size
+      #character_counter += word.size
+    else
+
+    end
+  end
   print text
+  print character_counter
 end
 
-input = read_from_user
-convert_to_array(input)
+
+read_from_user
