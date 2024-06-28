600b595c91a56a39b3cfc827bccf56d2de3823ab
shadai <shadai@movomx.com>
2024-06-28 11:31:50 -0700

    005 ready

--- 005.rb
+++ 005.rb
@@ -2,7 +2,7 @@
 # strings, tal que ninguno de sus elementos pase de los 140 caracteres.
 # Ninguna palabra debe quedar truncada.
 
-#Escriba un algoritmo que tome un texto del usuario y devuelva un array de strings, tal que ninguno de sus elementos pase de los 140 caracteres. Ninguna palabra debe quedar trunca. texto para rellenar la palabra hola hola
+#Escriba un algoritmo que tome un texto del usuario y devuelva un  array de strings, tal que ninguno de sus elementos pase de los 140 caracteres. Ninguna palabra debe quedar trunca. texto para rellenar la palabra hola hola
 
 def read_from_user
   print "Ingrese un texto: "
@@ -18,9 +18,10 @@ def create_text_array(array)
   array_characters = []
   array.each do |word|
     if is_max_characters?(new_string, word)
-      array_characters.push(new_string + " ")
-      new_string = word + " "
-    else new_string += word + " " end
+      array_characters.push(new_string)
+      new_string = ''
+    end
+    new_string += word + ' '
   end
   array_characters.push(new_string)
 end
@@ -29,7 +30,13 @@ def is_max_characters?(new_string, word)
   true if new_string.size + word.size + 1 > 140
 end
 
+def print_array(text_array)
+  text_array.each do |text|
+    puts text
+  end
+end
+
 user_text = read_from_user
 words_array = to_words_array(user_text)
 text_array = create_text_array(words_array)
-print text_array
+print_array(text_array)
