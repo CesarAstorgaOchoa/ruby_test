a3ac010cd32a034f9317af089d9e8dea9cbde82c
shadai <shadai@movomx.com>
2024-06-27 17:27:04 -0700

    tests 005.rb

--- 005.rb
+++ 005.rb
@@ -5,24 +5,29 @@
 def read_from_user
   input = 'Escriba un algoritmo que tome un texto del usuario y devuelva un array de strings, tal que ninguno de sus elementos pase de los 140 caracteres. Ninguna palabra debe quedar truncada, carateres de mas'
   #input = gets.chomp
-  check_array(input)
+  get_string(input)
 end
 
-def check_array(text)
-  text_array = text.split
-  character_counter = 0
-  text = ''
-  text_array.each do |word|
-    if character_counter < 140
-      character_counter = text.insert( -1, " #{word}").size
-      #character_counter += word.size
-    else
 
-    end
-  end
-  print text
-  print character_counter
-end
+
+# def get_string(text)
+#   words_array = text.split(/^.{0,30}\b/)
+#   print words_array
+# end
+
+# def check_array(text)
+#   text_array = text.split
+#   character_counter = 0
+#   text = ''s
+#   text_array.each do |word|
+#     if character_counter < 140
+#       character_counter = text.insert( -1, " #{word}").size
+#       #character_counter += word.size
+#     end
+#   end
+#   print text
+#   print character_counter
+# end
 
 
 read_from_user
