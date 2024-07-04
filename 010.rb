12398ffcf821015e28d5cf0b3404c8f5bc0308fc
shadai <shadai@movomx.com>
2024-07-04 14:15:53 -0700

    010, 011 ready

--- 010.rb
+++ 010.rb
@@ -1,3 +1,20 @@
 # Escriba un algoritmo que al recibir un valor númerico, devuelva si es
 # múltiplo de 3 y 5
 
+def read_from_user
+  print 'Ingrese un numero: '
+  input = gets.chomp.to_i
+end
+
+def multiple?(number)
+  number % 3 == 0 && number % 5 == 0
+end
+
+def print_result(result)
+  message = 'No es multiplo de ambos'
+  message = 'Si es multiplo de ambos' if multiple?(result)
+  puts message
+end
+
+number = read_from_user
+print_result(number)
--- 011.rb
+++ 011.rb
@@ -1,3 +1,16 @@
 # Escriba un algoritmo que al recibir un par de valores a y b representando los
 # catetos de un triángulo rectangulo, devuelva el valor de la hipotenusa.
+puts 'Ingrese le primer valor: '
+first_value = gets.chomp.to_i
 
+puts 'Ingrese el segundo valor: '
+second_value = gets.chomp.to_i
+
+def hypotenuse_value(first_value, second_value)
+  value_one = first_value**2
+  value_two = second_value**2
+  Math.sqrt(value_one+value_two)
+end
+
+hypotenuse = hypotenuse_value(first_value, second_value)
+puts hypotenuse
