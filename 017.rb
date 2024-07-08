8b5fe482b597fe274aa996055107b527bc5916d2
shadai <shadai@movomx.com>
2024-07-08 14:35:48 -0700

    017 ready

--- 017.rb
+++ 017.rb
@@ -1,2 +1,16 @@
 # Escriba un algoritmo que tome del usuario un número que represente un ángulo
 # en radianes y devuelva el equivalente en grados.
+
+def read_from_user
+  print 'Ingrese un angulo: '
+  input = gets.chomp.to_i
+end
+
+def grad_to_rad(grad)
+  grad * (180/Math::PI)
+end
+
+grad = read_from_user
+rad = grad_to_rad(grad)
+
+puts "Angulo en radianes #{rad}"
