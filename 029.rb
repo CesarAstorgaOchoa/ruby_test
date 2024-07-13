cfcd7b88f6f38dc139bce7d02e44b25c15014b87
shadai <shadai@movomx.com>
2024-07-12 17:49:18 -0700

    029

--- 029.rb
+++ 029.rb
@@ -3,3 +3,23 @@
 #
 # Ejemplo:
 #   (60, 136, 126) -> #3C887E
+
+def read_from_user
+  print 'Inserte el color en formato RGB: '
+  input = gets.chomp.to_s
+end
+
+def convert_to_hex(rgb)
+  array_rgb = rgb.scan(/\d+/)
+  array_rgb = array_rgb.map(&:to_i)
+  array_rgb.map { |value| value.to_s(16).upcase }
+end
+
+def print_result(result)
+  result.unshift('#')
+  puts result.join
+end
+
+rgb = read_from_user
+result = convert_to_hex(rgb)
+print_result(result)
