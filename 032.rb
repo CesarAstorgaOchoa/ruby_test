f3058a6baa484d8a5a48f461e76cb1cf52337a51
shadai <shadai@movomx.com>
2024-07-15 11:09:17 -0700

    032

--- 032.rb
+++ 032.rb
@@ -1,1 +1,13 @@
 # Imprima todos los números primos que hay entre el 1 y el 100
+
+def prime?(number)
+
+  return false if number <= 1
+
+  (2..number-1).each do |num|
+    return false if number % num == 0
+  end
+
+end
+
+(1..100).each { |num| puts num if prime?(num) }
