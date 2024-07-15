be6e8fd41c1d2c33caec15d7d7a919bd3f6c66f8
shadai <shadai@movomx.com>
2024-07-15 14:37:44 -0700

    034

--- 034.rb
+++ 034.rb
@@ -1,1 +1,20 @@
 # Imprima los primeros 100 números primos
+
+def prime?(number)
+
+  return false if number <= 1
+
+  (2..number-1).each do |num|
+    return false if number % num == 0
+  end
+
+end
+
+@max_limit = 100
+
+def limiter(number)
+  return false if @max_limit == 0
+  @max_limit -= 1 if prime?(number)
+end
+
+(1..1000).each { |num| puts num if limiter(num) }
