1a4d2020c87f59cc343f9b201bacc1bffbc857ba
shadai <shadai@movomx.com>
2024-07-15 11:18:13 -0700

    033

--- 033.rb
+++ 033.rb
@@ -1,1 +1,12 @@
 # Imprima todos los números que no son primos entre el 1 y el 100
+def prime?(number)
+
+  return true if number <= 1
+
+  (2..number-1).each do |num|
+    return false if number % num == 0
+  end
+
+end
+
+(1..100).each { |num| puts num unless prime?(num) }
