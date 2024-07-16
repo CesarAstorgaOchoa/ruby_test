fe16acc79ac20e910292e4fd6c5c9f4f66b7bd8b
shadai <shadai@movomx.com>
2024-07-16 14:24:54 -0700

    041

--- 040.rb
+++ 040.rb
@@ -2,3 +2,19 @@
 # en un torneo, organice de forma aleatoria la primer ronda de encuentros.
 
 @n = 4
+
+def create_players
+  (1..2**@n).to_a
+end
+
+def game(array_players)
+  team_a = array_players.sample(8)
+  team_b = array_players.difference(team_a)
+  print team_a
+  print team_b
+end
+
+
+
+array_players = create_players
+game(array_players)
--- 041.rb
+++ 041.rb
@@ -7,3 +7,15 @@
 # 2 X 5 = 10
 # ...
 # 10 X 5 = 50
+
+def read_from_user
+  print 'Ingrese un numero: '
+  input = gets.chomp.to_i
+end
+
+def generate_table(number)
+  (1..10).each { |v| puts " #{v} X #{number} = #{number * v} " }
+end
+
+number = read_from_user
+generate_table(number)
