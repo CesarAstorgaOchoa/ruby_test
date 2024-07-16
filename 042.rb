9f3c915387a1a3fbf4c0ccd0ed878805b91e31f9
shadai <shadai@movomx.com>
2024-07-16 14:32:45 -0700

    042

--- 042.rb
+++ 042.rb
@@ -1,2 +1,8 @@
 # Escriba un algoritmo que genere las tablas de múltiplicar de los números del
 # 1 al 10
+
+
+(1..10).each do |number|
+  (1..10).each { |mult| puts " #{number} X #{mult} = #{number * mult}" }
+  puts "\n"
+end
